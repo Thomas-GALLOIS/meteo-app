348b1c3e44799cd17ec6378ba4b5331eff7858cc
gallois <ttlp13@yahoo.fr>
2021-10-12 14:20:23 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,258 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="titre"><h1>Météo.app</h1>
+          </div>
+          <div class="choix">
+            <p>Saisissez votre ville :</p>
+              <input class="saisie" placeholder="votre ville" v-model="ville" @keyup.enter="meteoParVille">
+              <button class="bouton" @click="meteoParVille">valider</button>
+            <p>Ou</p>
+          <button class="boutonloc" @click="meteoParLoc">Utiliser la géolocalisation</button>
+          </div>
+        <div class="cadre-meteo">
+          <h1  class="ville">Météo à : {{ville}} </h1>
+          </div>  
+
+          <div :class="card">
+          
+          <Meteo
+            v-for="(element,index) in tableaumeteo"
+            :key="index"
+            :dateTime="element.dt_txt"
+            :temp="element.main.temp"
+            :tempMin="element.main.temp_min"
+            :tempMax="element.main.temp_max"
+            :speed="element.wind.speed"
+            :description="element.weather[0].description"
+            :icon="element.weather[0].icon"
+            />
+            
+            </div>
+          
+      
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Meteo from './components/meteo.vue'
 
 export default {
-  name: 'App',
+
+
   components: {
-    HelloWorld
+    Meteo: Meteo,
+  },
+
+  /* Data properties */
+  data() {
+    return {
+      ville: "",
+
+      tableaumeteo: [],
+      temperature: 0,
+      card: "",
+    };
+  },
+
+  /* Mounted */
+  async mounted() {
+    const response = await fetch(
+      "https://api.openweathermap.org/data/2.5/forecast?zip=06000,fr&appid=5f818b8bda9ed6f2b175b4aa0d1dc6e8&units=metric&lang=fr"
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+    this.ville = weatherData.city.name;
+    this.tableaumeteo = weatherData.list;
+    this.temperature = weatherData.list[0].main.temp;
+
+    if (this.temperature >= 25) {
+      this.card = "cardHot";
+    } else if (this.temperature >= 10 && this.temperature < 25) {
+      this.card = "card";
+    } else {
+      this.card = "cardCold";
+    }
+  },
+
+  methods: {
+    meteoParLoc() {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+
+        const response = await fetch(
+          "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+            lat +
+            "&lon=" +
+            lng +
+            "&appid=5f818b8bda9ed6f2b175b4aa0d1dc6e8&units=metric&lang=fr"
+        );
+
+        const weatherData = await response.json();
+        console.log(weatherData);
+
+        this.ville = weatherData.city.name;
+        this.tableaumeteo = weatherData.list;
+        this.temperature = weatherData.list[0].main.temp;
+
+        if (this.temperature >= 25) {
+          this.card = "cardHot";
+        } else if (this.temperature >= 10 && this.temperature < 25) {
+          this.card = "card";
+        } else {
+          this.card = "cardCold";
+        }
+      });
+    },
+
+    async meteoParVille() {
+      const response = await fetch(
+        "https://api.openweathermap.org/data/2.5/forecast?q=" +
+          this.ville +
+          "&appid=5f818b8bda9ed6f2b175b4aa0d1dc6e8&units=metric&lang=fr"
+      );
+      const weatherData = await response.json();
+      console.log(weatherData);
+      this.ville = weatherData.city.name;
+      this.tableaumeteo = weatherData.list;
+      this.temperature = weatherData.list[0].main.temp;
+
+      if (this.temperature >= 25) {
+        this.card = "cardHot";
+      } else if (this.temperature >= 10 && this.temperature < 25) {
+        this.card = "card";
+      } else {
+        this.card = "cardCold";
+      }
+    },
+  },
+
+  
+  
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-image: url('./assets/Beautiful-sky-clouds-sunset_m.jpeg');
+  background-size: cover;
+  color: white;
+  font-family: "Barlow", sans-serif;
+  font-weight: bold;
+  justify-content: center;
 }
+.titre {
+  font-size: 17px;
+  margin-left: 20px;
+}
+
+.choix {
+  display: flex;
+  justify-content: center;
+  font-size: 17px;
+  align-items: center;
+}
+
+.saisie {
+  padding: 5px;
+  margin-left: 5px;
+  margin-right: 5px;
+  border-radius: 5px;
+
+  width: 150px;
+  border: 2px solid rgb(189, 235, 243);
+  background-color: rgb(189, 235, 243);
+  color: #809bce;
+}
+.bouton {
+  height: 30px;
+  margin-left: 5px;
+  margin-right: 5px;
+  border-radius: 10px;
+  background-color: #809bce;
+  color: white;
+  width: 100px;
+  font-size: 17px;
+  box-shadow: 1px 3px 6px #444444;
+}
+.boutonloc {
+  height: 30px;
+  padding: 5px;
+  margin-left: 5px;
+  margin-right: 5px;
+  border-radius: 10px;
+  background-color: #809bce;
+  color: white;
+  width: 200px;
+  font-size: 15px;
+  box-shadow: 1px 3px 6px #444444;
+}
+.ville {
+  text-align: center;
+  color: white;
+  font-size: 40px;
+  font-weight: bold;
+}
+
+.card {
+  height: 483px;
+  width: 720px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 15px;
+
+  display: flex;
+  flex-direction: row;
+  overflow-x: scroll;
+  border-radius: 5px;
+  box-shadow: 4px 5px 8px #444444;
+  background-color: rgb(13, 110, 155);
+}
+
+.cardHot {
+  height: 483px;
+  width: 720px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 15px;
+
+  display: flex;
+  flex-direction: row;
+  overflow-x: scroll;
+  border-radius: 5px;
+  box-shadow: 4px 5px 8px #444444;
+  background-image: url("./assets/12-127551_fire-burning.jpeg");
+  background-size: cover;
+}
+.cardCold {
+  height: 483px;
+  width: 720px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 15px;
+
+  display: flex;
+  flex-direction: row;
+  overflow-x: scroll;
+  border-radius: 5px;
+  box-shadow: 4px 5px 8px #444444;
+  background-image: url("./assets/fond-ecran-wallpaper-image-fonds-ecran-abstrait-glace-18-660x330.jpeg");
+  background-size: cover;
+}
+
+.meteo {
+  margin: 20px 20px 20px 20px;
+  background-color: rgba(176, 176, 235, 0.5);
+  box-shadow: 4px 5px 8px #444444;
+  border-radius: 5px;
+  padding: 20px;
+  color: white;
+  width: 150px;
+  height: 400px;
+  font-size: 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
 </style>
---
+++ src/components/meteo.vue
@@ -0,0 +1,41 @@
+<template>
+ 
+  <div class="meteo">
+  <h3 class="date">{{dateTime}}</h3>
+  <p class="description">{{description}}</p>
+  <p>Température: <br/><br/>{{temp}}°c</p>
+  <p>Températures prévues: <br/><br/>Min: {{tempMin}}°c <br/>Max: {{tempMax}}°c</p>
+  <p>Vitesse du vent: {{speed}}km/h</p>
+  <img :src="'http://openweathermap.org/img/wn/' + icon + '@2x.png'" />
+  </div>
+
+      
+  
+</template>
+
+<script>
+export default {
+  
+  props: {
+    dateTime: String,
+    temp: Number,
+    tempMin: Number,
+    tempMax: Number,
+    speed: Number,
+    description: String,
+    icon: String,
+  },
+
+  
+};
+
+
+
+  
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
